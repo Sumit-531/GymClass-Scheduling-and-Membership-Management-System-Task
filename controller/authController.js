a14979c0774b1102725ab8ee02be0706e8e8b2f0
Sumit-531 <sumitchakraborty218@gmail.com>
2024-12-01 03:10:27 +0600

    Login Process

--- controller/authController.js
+++ controller/authController.js
@@ -1,5 +1,6 @@
 const user = require("../db/models/user");
 const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
 
 const generateToken = (payload) => {
     return jwt.sign(payload, process.env.JWT_SECRET_KEY, {
@@ -57,4 +58,36 @@ const signup = async (req, res, next) =>{
     });
 };
 
-module.exports = { signup };
+const login = async (req, res, next) => {
+    const {email, password} = req.body;
+
+    if(!email || !password) {
+        return res.status(400).json({
+            status: "fail",
+            message: "Please provide email and password.",
+        });
+    }
+
+    //fetch the user from database
+    const result = await user.findOne({where: { email }});
+    //compare email, password and match
+    if(!result || !(await bcrypt.compare(password, result.password))){
+        return res.status(401).json({
+            status: "fail",
+            message: "Incorrect email or password.",
+        });
+    }
+
+    // generate token
+    const token = generateToken({
+        id: result.id,
+    });
+
+    return res.json({
+        status: "success",
+        token,
+    });
+}
+
+
+module.exports = { signup, login };
